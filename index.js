61e5c813c61571a9d84a20ec18786401629a0b36
Jaden <jaden@bertinofamily.com>
2022-10-26 13:36:29 -0700

    Create initial project files

---
+++ index.js
@@ -0,0 +1,16 @@
+/* TODO
+https://londonappbrewery.github.io/Simon-Game/
+- initialize game (press a key to start)
+- wait for game start (user key press)
+
+- generate new step
+- display it (button animation + sound)
+- add to list
+
+- wait for user click
+- user click -> button animation + sound
+- check if they got it correct
+
+- if correct, then loop again
+- if wrong, then show game over screen (red flash, press any key to restart)
+*/
